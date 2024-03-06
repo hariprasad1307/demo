f1fae2d1e0541f208d2f5f569f2cf7a9b1660e62
hariprasad1307 <153909846+hariprasad1307@users.noreply.github.com>
2024-03-06 21:41:05 +0530

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,9 @@
+function show(){
+    var a=document.getElementById("nameinput").textContent
+    
+    console.log(a)
+}
+function show1(){
+    var b=document.getElementById("displayname")
+    
+}
