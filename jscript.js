ea2ed5793616569833c7f905b71846b35fc5cbc4
hariprasad1307 <153909846+hariprasad1307@users.noreply.github.com>
2024-03-07 18:39:41 +0530

    Delete jscript.js

--- jscript.js
+++
@@ -1,9 +0,0 @@
-function show(){
-    var a=document.getElementById("nameinput").textContent
-    
-    console.log(a)
-}
-function show1(){
-    var b=document.getElementById("displayname")
-    
-}
